65b277d08c6c4c6b79c35d35df5171c2cef911a8
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-12-15 22:06:10 -0300

    Implement react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 }
