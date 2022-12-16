0ee009c43f5d09ae26d5fe94e503db96c7df2e10
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-12-15 22:44:24 -0300

    Show the error message

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useRouteError } from "react-router-dom";
 
-function index() {
-  return <div>index</div>;
+function ErrorPage() {
+  const error = useRouteError();
+
+  return (
+    <div>
+      <h1>Oh no!</h1>
+      <p>This page isn't available, better go back home...</p>
+      <p>
+        <b>{error.message}</b>
+      </p>
+    </div>
+  );
 }
 
-export default index;
+export default ErrorPage;
