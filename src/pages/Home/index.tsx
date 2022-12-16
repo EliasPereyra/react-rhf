c5362f4e2b85f170ac6f90d5309ff40fd9215f0c
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-12-16 18:34:25 -0300

    Import the Form component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import FormComponent from "../../components/Form";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <h1>Do you want to get new talent?</h1>
+      <h3>We've got the best devs for you.</h3>
+      <p>You can filter devs by these categories: </p>
+
+      <FormComponent />
+    </main>
+  );
 }
 
 export default Home;
